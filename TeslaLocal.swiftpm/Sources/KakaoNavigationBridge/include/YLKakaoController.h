7f47e2de19461d214196b0afbeebb467dab70d49
brciae <brciae@gmail.com>
2026-09-22 23:34:12 +0900

    Document official SDK frequency mapping

--- TeslaLocal.swiftpm/Sources/KakaoNavigationBridge/include/YLKakaoController.h
+++ TeslaLocal.swiftpm/Sources/KakaoNavigationBridge/include/YLKakaoController.h
@@ -9,7 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)configureMapTheme:(NSString *)theme NS_SWIFT_NAME(configureMapTheme(_:));
 - (void)configureVoice:(BOOL)enabled safety:(BOOL)safety volume:(float)volume duck:(BOOL)duck
     NS_SWIFT_NAME(configureVoice(enabled:safety:volume:duck:));
-/// 0 = brief (turns close to the junction and real hazards), 1 = normal, 2 = everything the SDK offers.
+/// Official SDK frequency: 0 = Rare, 1 = Often, 2 = Always.
 - (void)configureVoiceDetail:(NSInteger)level NS_SWIFT_NAME(configureVoiceDetail(_:));
 - (void)prepareWithAppKey:(NSString *)key latitude:(double)latitude longitude:(double)longitude
      destinationLatitude:(double)destinationLatitude destinationLongitude:(double)destinationLongitude
@@ -17,8 +17,8 @@ NS_ASSUME_NONNULL_BEGIN
     NS_SWIFT_NAME(prepare(appKey:latitude:longitude:destinationLatitude:destinationLongitude:name:hipass:validUntil:));
 - (void)prepareStandbyWithAppKey:(NSString *)key latitude:(double)latitude longitude:(double)longitude
     NS_SWIFT_NAME(prepareStandby(appKey:latitude:longitude:));
-- (void)updateStandbyLocationWithLatitude:(double)latitude longitude:(double)longitude bearing:(double)bearing speed:(double)speed
-    NS_SWIFT_NAME(updateStandbyLocation(latitude:longitude:bearing:speed:));
+- (void)updateStandbyLocationWithLatitude:(double)latitude longitude:(double)longitude bearing:(double)bearing speed:(double)speed timestamp:(double)timestamp
+    NS_SWIFT_NAME(updateStandbyLocation(latitude:longitude:bearing:speed:timestamp:));
 - (void)stopNavigation NS_SWIFT_NAME(stopNavigation());
 /// Resume camera tracking after manual map browsing.
 - (void)recenter NS_SWIFT_NAME(recenter());
